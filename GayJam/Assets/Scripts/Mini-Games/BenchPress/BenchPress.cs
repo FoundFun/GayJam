03ce7a879e3655b4186f180bce2eaff6b2811ef7
Anatoly Ponomarev <i@foundfun.ru>
2024-06-29 20:43:58 +0500

    Add bars benchPress

--- GayJam/Assets/Scripts/Mini-Games/BenchPress/BenchPress.cs
+++ GayJam/Assets/Scripts/Mini-Games/BenchPress/BenchPress.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using Mini_Games.PullUp;
 using Player;
 using TMPro;
@@ -11,5 +13,50 @@ namespace Mini_Games.BenchPress
         [SerializeField] private TMP_Text _approach;
         [SerializeField] private PullUpConfig _config;
         [SerializeField] private PlayerMover _playerMover;
+        [SerializeField] private List<BenchPressBar> _bars;
+
+        private int _count;
+
+        private void OnEnable()
+        {
+            foreach (BenchPressBar bar in _bars)
+            {
+                bar.NextBar += OnNextBar;
+            }
+            
+            foreach (BenchPressBar pressBar in _bars)
+            {
+                pressBar.gameObject.SetActive(false);
+            }
+
+            _count = 0;
+            _bars[_count].gameObject.SetActive(true);
+        }
+
+        private void OnDisable()
+        {
+            foreach (BenchPressBar bar in _bars)
+            {
+                bar.NextBar -= OnNextBar;
+            }
+        }
+
+        private void OnNextBar()
+        {
+            StartCoroutine(NextBar());
+        }
+
+        private IEnumerator NextBar()
+        {
+            yield return new WaitForSeconds(1);
+
+            foreach (BenchPressBar pressBar in _bars)
+            {
+                pressBar.gameObject.SetActive(false);
+            }
+
+            _count++;
+            _bars[_count].gameObject.SetActive(true);
+        }
     }
 }
